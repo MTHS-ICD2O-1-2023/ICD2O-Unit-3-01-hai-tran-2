ce39cff8a632a3f4ed5d6b070920c14652f496b4
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-28 18:45:55 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateArea() {
+  // input
+  const baseAOfTrapezoid = parseFloat(document.getElementById('base-a').value)
+  const baseBOfTrapezoid = parseFloat(document.getElementById('base-b').value)
+  const heightOfTrapezoid = parseFloat(document.getElementById('height').value)
+
+  // process
+  const areaOfTrapezoid = (baseAOfTrapezoid + baseBOfTrapezoid) / 2 * heightOfTrapezoid
+  // output
+  document.getElementById('answer').innerHTML = "The area is: " + areaOfTrapezoid + " mm²"
+}
